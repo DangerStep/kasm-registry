0c9e40124200fd2a2c895262e5614f9e1ece36b0
DangerStep <103132813+DangerStep@users.noreply.github.com>
2024-03-01 13:17:20 -0800

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,10 +2,10 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Kai's Repos',
+    description: 'The repos that I have added',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://dangerstep.github.io/kasm-registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
